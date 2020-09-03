83372fcd16a811589e3f10a22bed21609a729781
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-03 23:17:21 +0100

    object literals

---
+++ topics/objects/object-literals.js
@@ -0,0 +1,14 @@
+// Object Literals is a simple way to define an object
+
+const circle = {
+	radius: 1,
+	location: {
+		x: 1,
+		y: 2,
+	},
+	draw: function () {
+		console.log('drawing');
+	},
+};
+
+circle.draw();
