15a5e5eb0bbde48e5f914f1120cdfd2c1ff4fd0d
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-03 23:11:16 +0100

    four pillars of OOP

---
+++ topics/four-pillar-of-OOP.js
@@ -0,0 +1,38 @@
+/*
+  1. Encapsulation
+  2. Abstraction
+  3. Inheritance
+  4. Polymorphism
+
+  Before OOP, we have Procedural Programming, it's very easy straightforward
+  But very difficult to manage, many inter-dependncies
+  They are called Spaghetti code
+
+  OOP combines a group of related variables and functions into a unit
+  This units are called OBJECTs
+  Variables are referred to as PROPERTIES and the functions as METHODs
+
+  Example, a car object has
+  properties: make, model, color, year
+  methods: start, stop, move
+
+  -ENCAPSULATION: In OOP, we group related variables and functions that operate on them into
+  a objects, and this is what we call ENCAPSULATION
+    **Benefits
+    - Reduce complexity
+    - increase reusability
+
+  - ABSTRACTION: this is hiding most of the complexity of your objects from
+  the outside world. 
+    **Benefits
+    - This makes the interface easier than an objects with several
+    methods and properties,
+    - Reduces the impact of change, because changing one part of the complex parts
+      does not leak or affect other parts
+
+  - INHERITANCE: this is a mechanism that allow us to eliminate redundant codes
+
+  - POLYMORPHISM: Poly means Many, Morphism means Form. This helps us eliminate 
+    long lines of IFs and SWITCH statement. We can have just one methods that behave
+    differently depending on the value of the property
+*/
