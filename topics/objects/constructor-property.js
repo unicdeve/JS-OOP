be8e3b0f82e1e92ccc23d732046b279ce3601b0e
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-03 23:57:01 +0100

    the constructor property

---
+++ topics/objects/constructor-property.js
@@ -0,0 +1,16 @@
+/*
+  Every object in JS have a constructor property,
+  and that references the function that was used to construct or create that object
+
+  when we use object literal e.g
+  let x = {}
+  JS essentially does let x = new Object()
+
+  same as other constructors like
+  new String() ---but we mostly use the string literals; '', "", ``
+  new Boolean --- boolean literals; true or false
+  new Number() --- number literals; 0, 1, 5 etc
+  new Function() --- function literals; e.g function createCircle() {}
+
+  Functions are also objects
+*/
