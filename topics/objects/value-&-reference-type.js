2d892f524ca37c3172a543de3f90d43a134fa9b1
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-04 00:09:39 +0100

    value and reference types

---
+++ topics/objects/value-&-reference-type.js
@@ -0,0 +1,21 @@
+/*
+  Primitive/Value Types
+    Number
+    String
+    Boolean
+    Symbol
+    undefined
+    null
+
+  Reference Types
+    Object
+    Function
+    Array
+
+  values of Value types variable are stored in that variale
+  but in reference types, the value is not stored in that variable but in
+  a memory location
+
+  Primitives are copied by value
+  Objects are copied by their reference
+*/
