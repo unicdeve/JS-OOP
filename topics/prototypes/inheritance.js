a60d73fb2391fdac3ebb5f6c5d42cd5ff7c09b64
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-04 10:56:46 +0100

    inheritance

---
+++ topics/prototypes/inheritance.js
@@ -0,0 +1,14 @@
+/*
+  INHERITANCE allows a object to inherit another object's methods and properties
+  For example, 
+  Circle, Square, Triangle can inherit from a Shape Object
+  The Shape object is referred to as Base/Super/Parent object
+  Circle, Square are referred to as Derived/Sub/Child object
+
+  So a Classical definition of inheritance would be IS-A, i.e 
+  Circle is-a Shape, Square is-a Shape
+
+  But in JavaScript we don't have the concept of Class, this is where Prototypical Inheritance comes in
+  So in JS, we have 2 types of inheritance
+  Classical and Prototypical Inheritance
+*/
