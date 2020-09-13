fd4f82f56ca951e9a112dac3c4892d04d50b46bc
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-13 23:33:28 +0100

    function hoisting

---
+++ topics/ES6-classes/hoisting.js
@@ -0,0 +1,21 @@
+// Function Declaration
+function sayHello() {}
+
+// Function expression
+const sayGoodMorning = function () {};
+
+/*
+  the difference between Function Declarations and Function expressions is that
+  Function Declarations are hoisted i.e moved to the TOP at runtime
+  while Function expressions are not
+*/
+
+// Class Declaration
+class Circle {}
+
+// Class expression
+const Circle = class {};
+
+/*
+  Unlike Functions, classes are not hoisted
+*/
