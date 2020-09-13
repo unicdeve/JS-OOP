2ba787503cb155d38edd7225976f244dad5ae48e
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-13 20:18:47 +0100

    resetting the constructor as well

--- topics/prototypical-inheritance/inheritance.js
+++ topics/prototypical-inheritance/inheritance.js
@@ -8,8 +8,24 @@ function Circle(radius) {
 	this.radius = radius;
 }
 
-// making Circle inherit from Shape
+/*
+	reseting Circle prototype, making Circle inherit from Shape
+	initially
+		Circle.prototype = Object.create(Object.prototype) // implicit relation
+		i.e the objectBase
+*/
 Circle.prototype = Object.create(Shape.prototype);
+
+/*
+	initially
+		Circle.prototype.constructor = Circle
+	reseting Circle prototype also reset it's constructor to the Shape constructor,
+	so we can no longer create a Circle dynamically with it's contructor function
+	Now,
+		Circle.prototype.constructor = Shape
+*/
+// as best practice, also reset the child constructor property back to the child's
+Circle.prototype.constructor = Circle;
 
 Circle.prototype.draw = function () {
 	console.log('draw');
