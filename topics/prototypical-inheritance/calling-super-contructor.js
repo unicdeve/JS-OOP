1a58f5b11551880e8ce65f0fb5151ef54674cf5e
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-09-13 20:26:06 +0100

    calling super constructor

---
+++ topics/prototypical-inheritance/calling-super-contructor.js
@@ -0,0 +1,23 @@
+function Shape(color) {
+	this.color = color;
+}
+
+Shape.prototype.duplicate = function () {
+	console.log('duplicate');
+};
+
+function Circle(radius, color) {
+	// calling super constructor
+	Shape.call(this, color);
+
+	this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+
+Circle.prototype.draw = function () {
+	console.log('draw');
+};
+
+const s = new Shape();
+const c = new Circle();
